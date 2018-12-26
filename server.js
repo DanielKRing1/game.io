a513680c1b8f5fb2d474ba80d92f8748bd85a81d
DanielKRing1 <DanielKRing1@gmail.com>
2018-12-26 04:33:17 -0800

    test

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ setInterval(() => {
 
         io.to(`${id}`).emit('state', nearbyPlayers, nearbyFood);
     });
-}, 1000 / 60);
+}, 1000 / 30);
 
 // -------- DELEGATE TASKS TO CLIENT-SIDE --------
 // For each connected socket, ask for collision check
