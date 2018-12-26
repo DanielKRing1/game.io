765582eede5f16dbfc2ee69636e289f42820d444
DanielKRing1 <DanielKRing1@gmail.com>
2018-12-26 04:36:04 -0800

    test

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) => {
     // New Player
     socket.on('new player', () => {
         // board.restartBoard();
-        for(let i = 0; i < 100; i++) {
+        for(let i = 0; i < 0; i++) {
             board.addPlayer(`${i}`, board.getRandomPos());
         }
         board.addPlayer(socket.id, {x:0, y:0});
